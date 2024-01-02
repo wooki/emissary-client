f48dcc577c90e906ac00556ce37ea70d3440d55a
James Rowe <jim@jimcode.org>
2024-01-02 17:08:04 +0000

    Update 5 files

--- src/components/AreaPanel.vue
+++ src/components/AreaPanel.vue
@@ -8,66 +8,101 @@
       <div v-if="selectedReport" class="area-panel-details">
         <button class="icon" @click="SelectReport(null)"><BackIcon /></button>
         <div class="area-panel-report-title">{{ selectedReport.type }}</div>
-        <div class="area-panel-report-details-row" v-for="(item, itemIndex) in selectedReport.info" :key="itemIndex">
-            <!-- <div class="area-panel-report-details-type">{{ item.type }}</div> -->
-            <div class="area-panel-report-details-message">{{ item.message }}</div>
-            <div class="area-panel-report-details-data" v-for="(dataKey, dataKeyIndex) in Object.keys(item.data)" :key="dataKeyIndex">
-              <div class="area-panel-report-entry-title">{{ dataKey }}</div>
-              <div class="area-panel-report-entry-value" :class="{negative: item.data[dataKey] < 0, positive: item.data[dataKey] > 0}">{{ item.data[dataKey] }}</div>
-            </div> 
-          </div>  
+        <div
+          class="area-panel-report-details-row"
+          v-for="(item, itemIndex) in selectedReport.info"
+          :key="itemIndex"
+        >
+          <!-- <div class="area-panel-report-details-type">{{ item.type }}</div> -->
+          <div class="area-panel-report-details-message">
+            {{ item.message }}
+          </div>
+          <div
+            class="area-panel-report-details-data"
+            v-for="(dataKey, dataKeyIndex) in Object.keys(item.data)"
+            :key="dataKeyIndex"
+          >
+            <div class="area-panel-report-entry-title">{{ dataKey }}</div>
+            <div
+              class="area-panel-report-entry-value"
+              :class="{
+                negative: item.data[dataKey] < 0,
+                positive: item.data[dataKey] > 0,
+              }"
+            >
+              {{ item.data[dataKey] }}
+            </div>
+          </div>
+        </div>
       </div>
 
       <div v-else class="area-panel-reports-container">
-        <div tabindex="0" @click="SelectReport(reportIndex)" @keypress.enter="SelectReport(reportIndex)" v-for="(report, reportIndex) in aggregateReports" :key="reportIndex" class="area-panel-report">
-
+        <div
+          tabindex="0"
+          @click="SelectReport(reportIndex)"
+          @keypress.enter="SelectReport(reportIndex)"
+          v-for="(report, reportIndex) in aggregateReports"
+          :key="reportIndex"
+          class="area-panel-report"
+        >
           <div v-if="report.base != null" class="area-panel-report-base">
             <div class="area-panel-report-title">{{ report.type }}</div>
-            <div class="area-panel-report-value">{{  report.base }}</div>
-            <div v-if="report.adjustment[0] != 0" class="area-panel-report-adjustment" :class="{negative: report.adjustment[0] < 0}">{{  report.adjustment[0].toFixed(2) }}</div>
+            <div class="area-panel-report-value">{{ report.base }}</div>
+            <div
+              v-if="report.adjustment[0] != 0"
+              class="area-panel-report-adjustment"
+              :class="{ negative: report.adjustment[0] < 0 }"
+            >
+              {{ report.adjustment[0].toFixed(2) }}
+            </div>
           </div>
           <div v-else class="area-panel-report-entries">
             <div class="area-panel-report-title">{{ report.type }}</div>
-            <div class="area-panel-report-entry" v-for="(entry, entryIndex) in Array.from(report.values.entries())" :key="entryIndex">
+            <div
+              class="area-panel-report-entry"
+              v-for="(entry, entryIndex) in Array.from(report.values.entries())"
+              :key="entryIndex"
+            >
               <div class="area-panel-report-entry-title">{{ entry[0] }}</div>
-              <div class="area-panel-report-entry-value" :class="{negative: entry[1] < 0, positive: entry[1] > 0}">{{ entry[1] }}</div>
+              <div
+                class="area-panel-report-entry-value"
+                :class="{ negative: entry[1] < 0, positive: entry[1] > 0 }"
+              >
+                {{ entry[1] }}
+              </div>
             </div>
-          </div>                    
+          </div>
         </div>
       </div>
     </div>
 
     <div class="area-panel-units">
       <div class="parchment"></div>
-
-    </div>    
-     
-    
+    </div>
   </div>
 </template>
 
 <script>
-import AreaPanelSummary from './AreaPanelSummary.vue'
-import { AddFloats } from '@/libs/SafeMath.js'
-import BackIcon from '@/assets/icons/back.svg'
-
+import AreaPanelSummary from "./AreaPanelSummary.vue";
+import { AddFloats } from "@/libs/SafeMath.js";
+import BackIcon from "@/assets/icons/back.svg";
 
 export default {
   props: {
     area: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
-  data: function() {
+  data: function () {
     return {
-      selectedReportIndex: null
+      selectedReportIndex: null,
     };
-  },  
+  },
   emits: ["select"],
-  components: {    
+  components: {
     AreaPanelSummary,
-    BackIcon
+    BackIcon,
   },
   computed: {
     selectedReport() {
@@ -77,30 +112,31 @@ export default {
       return reportsArray[this.selectedReportIndex];
     },
     aggregateReports() {
-      
       const reports = new Map();
 
       if (!this.area.info) return [];
 
       // one per type and one per store item, iterate and create or update
       this.area.info.forEach((info) => {
-        reports.set(info.type, this.CreateOrAddToReport(reports, info, this.area));
-      });     
-      
-      reports.forEach((report, reportKey, rpts) => {
+        reports.set(
+          info.type,
+          this.CreateOrAddToReport(reports, info, this.area),
+        );
+      });
 
+      reports.forEach((report, reportKey, rpts) => {
         const valueKeys = Array.from(report.values.keys());
         if (valueKeys.length == 1) {
           const baseKey = valueKeys[0];
           if (this.area[baseKey] != null) {
             report.base = this.area[baseKey];
             report.adjustment = Array.from(report.values.values());
-          }      
+          }
         }
       });
 
       return Array.from(reports.values());
-    }
+    },
   },
   methods: {
     SelectArea(area) {
@@ -116,15 +152,15 @@ export default {
     CreateOrAddToReport(reports, info) {
       let report = {};
       if (reports.has(info.type)) {
-        report = reports.get(info.type);        
+        report = reports.get(info.type);
       } else {
         report = {
           type: info.type,
           info: [],
           values: new Map(),
           base: null,
-          adjustment: null
-        }                
+          adjustment: null,
+        };
       }
 
       // add info and values
@@ -132,208 +168,222 @@ export default {
 
       Object.keys(info.data).forEach((key) => {
         if (report.values.has(key)) {
-          report.values.set(key, AddFloats(report.values.get(key), info.data[key], 4));
+          report.values.set(
+            key,
+            AddFloats(report.values.get(key), info.data[key], 4),
+          );
         } else {
           report.values.set(key, info.data[key]);
-        }        
-      });      
-      
+        }
+      });
+
       return report;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .area-panel {
-    display: grid;
-    grid-template-columns: 1fr 3fr 3fr;       
-    grid-template-rows: 1fr; 
-    gap: 12px;
-    height: 100%;        
+.area-panel {
+  display: grid;
+  grid-template-columns: 1fr 3fr 3fr;
+  grid-template-rows: 1fr;
+  gap: 12px;
+  height: 100%;
+}
+
+.area-panel-summary {
+  position: relative;
+  min-width: 200px;
+  background-color: transparent;
+  color: var(--color-text);
+}
+
+.area-panel-reports {
+  position: relative;
+  z-index: 1;
+  background-color: transparent;
+  color: var(--color-text);
+}
+
+.area-panel-units {
+  position: relative;
+  z-index: 1;
+  background-color: transparent;
+  color: var(--color-text);
+}
+
+.area-panel-reports-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px 24px;
+  padding: 7px 7px;
+}
+
+.area-panel-report {
+  flex: 1 1 80px;
+  max-width: 120px;
+  font-size: 12px;
+  cursor: pointer;
+  padding: 4px;
+  border-radius: 2px;
+  border: 1px solid transparent;
+  transition: all 100ms ease;
+}
+.area-panel-report:hover,
+.area-panel-report:focus-visible {
+  background: rgba(255, 255, 255, 0.1);
+  border: 1px solid var(--color-text);
+}
+
+.area-panel-report-title {
+  font-weight: bold;
+}
+
+.area-panel-report-base {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2px 5px;
+
+  .area-panel-report-title {
+    width: 100%;
   }
 
-  .area-panel-summary {
-    position: relative;
-    min-width: 200px;
-    background-color: transparent;    
+  .area-panel-report-value {
+    font-size: 28px;
+    line-height: 1;
+  }
+  .area-panel-report-adjustment {
+    font-family: "Courier New", Courier, monospace;
+    line-height: 1.4;
+    color: var(--color-green);
+    &::before {
+      content: "+";
+    }
+    &.negative {
+      color: var(--color-red);
+      &::before {
+        content: "";
+      }
+    }
+  }
+}
+
+.area-panel-report-entry {
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+
+  .area-panel-report-entry-value {
+    font-family: "Courier New", Courier, monospace;
+    text-align: right;
+    line-height: 1.3;
+    &.positive {
+      color: var(--color-green);
+      &::before {
+        content: "+";
+      }
+    }
+    &.negative {
+      color: var(--color-red);
+      &::before {
+        content: "";
+      }
+    }
+  }
+}
+
+.area-panel-details {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 12px;
+  font-size: 12px;
+  overflow: auto;
+
+  button {
+    position: absolute;
+    top: 3px;
+    right: 6px;
+    height: 32px;
+    width: 32px;
+    border: 1px solid transparent;
+    background-color: transparent;
     color: var(--color-text);
-  }
 
-  .area-panel-reports {
-    position: relative;
-    z-index: 1;
-    background-color: transparent;    
-    color: var(--color-text);
-  }
-
-  .area-panel-units {
-    position: relative;
-    z-index: 1;
-    background-color: transparent;    
-    color: var(--color-text);
-  }
-
-  .area-panel-reports-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 12px 24px;
-    padding: 7px 7px;
-  }
-
-  .area-panel-report {
-    flex: 1 1 80px;
-    max-width: 120px;
-    font-size: 12px;
-    cursor: pointer;
-    padding: 4px;
-    border-radius: 2px;
-    border: 1px solid transparent;    
-    transition: all 100ms ease;
-  }
-  .area-panel-report:hover, .area-panel-report:focus-visible {
-    background: rgba(255, 255, 255, 0.1);
-    border: 1px solid var(--color-text);
+    &:hover,
+    &:focus-visible {
+      background: rgba(255, 255, 255, 0.1);
+      border: 1px solid var(--color-text);
+    }
   }
 
   .area-panel-report-title {
+    margin: 0 0 1em 0;
+  }
+
+  .area-panel-report-details-row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    margin-bottom: 0.25em;
+    padding-bottom: 0.25em;
+    font-size: 14px;
+    gap: 6px;
+    align-items: center;
+    border-bottom: 1px solid var(--color-text);
+  }
+
+  .area-panel-report-details-row:has(.area-panel-report-details-data) {
+    .area-panel-report-details-message {
+      padding-top: 13px;
+    }
+  }
+
+  .area-panel-report-details-message {
+    margin-right: auto;
+  }
+
+  .area-panel-report-entry-title {
+    font-size: 12px;
     font-weight: bold;
   }
+  .area-panel-report-entry-value {
+    text-align: right;
 
-  .area-panel-report-base {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 2px 5px;    
-
-    .area-panel-report-title {
-      width: 100%;
-    }
-    
-    .area-panel-report-value {
-      font-size: 28px;
-      line-height: 1;      
-    }
-    .area-panel-report-adjustment {
-      font-family: 'Courier New', Courier, monospace;
-      line-height: 1.4;
+    &.positive {
       color: var(--color-green);
       &::before {
-        content: "+"
-      }
-      &.negative {
-        color: var(--color-red);  
-        &::before {
-        content: ""
-      }
+        content: "+";
       }
     }
-  }
-
-  .area-panel-report-entry {
-    display: grid;
-    grid-template-columns: 3fr 2fr;
-
-    .area-panel-report-entry-value {
-      font-family: 'Courier New', Courier, monospace;
-      text-align: right;
-      line-height: 1.3;
-      &.positive {
-        color: var(--color-green);
-        &::before {
-          content: "+"
-        }
-      }
-      &.negative {
-        color: var(--color-red);  
-        &::before {
-        content: ""
-      }
+    &.negative {
+      color: var(--color-red);
+      &::before {
+        content: "";
       }
     }
   }
+}
 
-  .area-panel-details {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    padding: 12px;
-    font-size: 12px;
-
-    button {
-      position: absolute;
-      top: 0px;
-      right: 6px;
-      background-color: transparent;
-      color: var(--color-text);
-    }
-
-    .area-panel-report-title {
-      margin: 0 0 1em 0;
-    }
-
-    .area-panel-report-details-row {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: nowrap;
-      margin-bottom: 0.25em;      
-      padding-bottom: 0.25em;
-      font-size: 14px;
-      gap: 6px;
-      align-items: center;
-      border-bottom: 1px solid var(--color-text);
-    }
-
-    .area-panel-report-details-row:has(.area-panel-report-details-data) {
-      .area-panel-report-details-message {
-        padding-top: 13px;
-      }
-    }
-
-    .area-panel-report-details-message {
-      margin-right: auto;
-    }
-
-    .area-panel-report-entry-title {
-      font-size: 12px;
-      font-weight: bold;
-    }
-    .area-panel-report-entry-value {
-      text-align: right;
-
-      &.positive {
-        color: var(--color-green);
-        &::before {
-          content: "+"
-        }
-      }
-      &.negative {
-        color: var(--color-red);  
-        &::before {
-          content: ""
-        }
-      }  
-    }  
+@media (min-aspect-ratio: 1.2/1) {
+  .area-panel {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 3fr 3fr;
+    gap: 12px;
+  }
+  .area-panel-summary {
+    min-width: unset;
+    min-height: 100px;
   }
 
-  @media (min-aspect-ratio: 1.2/1) {
-    .area-panel {
-      grid-template-columns: 1fr;
-      grid-template-rows: 1fr 3fr 3fr;      
-      gap: 12px;
-    }
-    .area-panel-summary {
-      min-width: unset;    
-      min-height: 100px;
-    }
-
-    .area-panel-info {
-      min-height: 150px;
-    }
-
-    .area-panel-features {           
-      min-height: 150px;          
-    }
+  .area-panel-info {
+    min-height: 150px;
   }
+
+  .area-panel-features {
+    min-height: 150px;
+  }
+}
 </style>
--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,82 +1,78 @@
 <template>
   <button class="link" @click="Click(coord)">
-    <ProvinceIcon />
     <div class="coord" v-if="showCoord || !showName || !name">{{ coord }}</div>
     <div class="name" v-if="showName && name">{{ name }}</div>
   </button>
 </template>
 
 <script>
-import ProvinceIcon from '@/assets/icons/town.svg'
-
 export default {
   props: {
     x: {
       type: Number,
-      required: true
+      required: true,
     },
     y: {
       type: Number,
-      required: true
+      required: true,
     },
     name: {
       type: String,
-      default: ''
+      default: "",
     },
     stroke: {
       type: String,
-      default: '#000'
+      default: "#000",
     },
     showCoord: {
       type: Boolean,
-      default: false
+      default: false,
     },
     showName: {
       type: Boolean,
-      default: true
+      default: true,
     },
-    showIcon: {
-      type: Boolean,
-      default: true
-    }    
-  },
-  components: {
-    ProvinceIcon
   },
   emits: ["click"],
   computed: {
     coord() {
-      return `${this.x},${this.y}`
-    }
+      return `${this.x},${this.y}`;
+    },
   },
   methods: {
     Click() {
-      this.$emit("click", {x: this.x, y: this.y});
-    }
-  }
-}
+      this.$emit("click", { x: this.x, y: this.y });
+    },
+  },
+};
 </script>
 
 <style scoped>
-  button.link {
-    background: transparent;
-    padding: 0;
-    color: var(--color-text);
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    line-height: 1.3;
-    gap: 0.3em;
-    font-weight: unset;
-    height: auto;
-
-    &:has(svg) {
-      gap: 0;
-    }
-    svg {
-      transform: translateY(-7%);
-      height: 1.2em;
-      width: 1.2em;
-    }
+button.link {
+  background: transparent;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  line-height: 1.3;
+  gap: 0.3em;
+  font-weight: unset;
+  height: auto;
+  border: none;
+  color: var(--color-link);
+  padding: 0 2px;
+  border-radius: 2px;
+  border: 1px solid transparent;
+  transition: all 100ms ease;
+  transform: translateX(-3px) translateY(-1px);
+  .name {
+    font-weight: bold;
   }
+
+  &:hover,
+  &:focus-visible {
+    background: rgba(255, 255, 255, 0.1);
+    border: 1px solid var(--color-text);
+  }
+}
 </style>
