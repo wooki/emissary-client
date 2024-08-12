3e0932d696b0ab622f679abbdd45f8a7fba48228
Jim Rowe <jim@jimcode.org>
2024-08-12 17:17:38 +0100

    move area data component

--- src/components/AreaPanel.vue
+++ src/components/AreaPanel.vue
@@ -1,15 +1,16 @@
 <template>
   <div v-if="report.isLoaded && report.selectedHex" class="area-panel">
     <AreaSummary />
-    <!-- <AreaPanelData></AreaPanelData>      
+    <AreaData />
+    <!-- 
     <AreaPanelItems></AreaPanelItems>       -->
   </div>
 </template>
 
 <script setup>
 import AreaSummary from './panels/AreaSummary.vue';
-import AreaPanelData from './AreaPanelData.vue';
-import AreaPanelItems from './AreaPanelItems.vue';
+import AreaData from './panels/AreaData.vue';
+// import AreaPanelItems from './AreaPanelItems.vue';
 
 import { useReportStore } from '@/stores/ReportStore';
 
